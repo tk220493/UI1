91b523c81c183f34338cbabfe2fe5b0071d52fa4
KonoeToshiaki <134259500+tk220493@users.noreply.github.com>
2024-06-06 06:49:18 +0900

    Delete TK220493_木衛俊明_UI1 directory

--- TK220493_木衛俊明_UI1/js/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const nav = document.querySelector('.nav');
-
-    hamburger.addEventListener('click', () => {
-        nav.classList.toggle('active');
-    });
-});
